cc31d3f13a6398132a5071285646b609618728d4
virgilin <virgilin@blus.com>
2019-03-01 16:16:32 +0800

    first commit

---
+++ concurrency/src/main/java/com/javaedge/concurrency/example/aqs/CountDownLatchExample2.java
@@ -0,0 +1,45 @@
+package com.javaedge.concurrency.example.aqs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class CountDownLatchExample2 {
+
+    private final static int threadCount = 200;
+
+    public static void main(String[] args) throws Exception {
+
+        ExecutorService exec = Executors.newCachedThreadPool();
+
+        final CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            final int threadNum = i;
+            exec.execute(() -> {
+                try {
+                    test(threadNum);
+                } catch (Exception e) {
+                    log.error("exception", e);
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+        /**
+         * 和await()类似，只不过等待一定的时间后count值还没变为0的话就会继续执行
+         */
+        countDownLatch.await(10, TimeUnit.MILLISECONDS);
+        log.info("finish");
+        exec.shutdown();
+    }
+
+    private static void test(int threadNum) throws Exception {
+        Thread.sleep(100);
+        log.info("{}", threadNum);
+    }
+}
